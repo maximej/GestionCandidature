94303c81065a007191f520709547488d4a4fb62a
maragatham <mara_damo@yahoo.fr>
2020-05-05 16:53:03 +0200

    Modif DB

--- src/main/java/com/GeekJob/concoursDEV/concours.java
+++ src/main/java/com/GeekJob/concoursDEV/concours.java
@@ -13,12 +13,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.sql.rowset.serial.SerialBlob;
 import javax.sql.rowset.serial.SerialException;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
+@Table(name = "Concours") 
 public class concours {
 	
 	@Id
